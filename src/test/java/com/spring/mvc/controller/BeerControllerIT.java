6da44c7c9fef6d3c9e33667d1105ed060764e611
damiang <damiangildea12@gmail.com>
2025-03-08 11:07:49 +0000

    Added tests for beer by ID - and Not found exception for a random UUID

--- src/test/java/com/spring/mvc/controller/BeerControllerIT.java
+++ src/test/java/com/spring/mvc/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package com.spring.mvc.controller;
 
+import com.spring.mvc.entities.Beer;
 import com.spring.mvc.model.BeerDTO;
 import com.spring.mvc.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -9,8 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -19,6 +22,23 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().getFirst();
+
+        BeerDTO dto = beerController.getBeerById(beer.getId());
+
+        assertThat(dto).isNotNull();
+    }
 
     @Test
     void testListBeers() {
