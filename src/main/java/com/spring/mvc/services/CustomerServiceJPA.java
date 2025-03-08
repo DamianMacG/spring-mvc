c3ab0359b7e1b6805f046d5f32682cc0244f824a
damiang <damiangildea12@gmail.com>
2025-03-08 10:26:06 +0000

    Set up JPA services for beer and customer

---
+++ src/main/java/com/spring/mvc/services/CustomerServiceJPA.java
@@ -0,0 +1,51 @@
+package com.spring.mvc.services;
+
+import com.spring.mvc.mappers.CustomerMapper;
+import com.spring.mvc.model.CustomerDTO;
+import com.spring.mvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
