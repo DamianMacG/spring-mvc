1b51f23ba177f0468eea5c141044c03c7cecc498
damiang <damiangildea12@gmail.com>
2025-03-08 10:59:24 +0000

    Added beer controller integration tests

---
+++ src/test/java/com/spring/mvc/controller/BeerControllerIT.java
@@ -0,0 +1,39 @@
+package com.spring.mvc.controller;
+
+import com.spring.mvc.model.BeerDTO;
+import com.spring.mvc.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+
+    @Rollback // Makes sure database changes made do not persist
+    @Transactional // Wraps test in a transaction so that any operations can use rollback
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(0);
+
+    }
+}
