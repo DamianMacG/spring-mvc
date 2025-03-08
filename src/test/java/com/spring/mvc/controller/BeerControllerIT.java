f293fe88007a456486f908cb2c5cabd2e6532b0a
damiang <damiangildea12@gmail.com>
2025-03-08 12:28:33 +0000

    Added updateExistingBeer happy path integration test

--- src/test/java/com/spring/mvc/controller/BeerControllerIT.java
+++ src/test/java/com/spring/mvc/controller/BeerControllerIT.java
@@ -1,6 +1,7 @@
 package com.spring.mvc.controller;
 
 import com.spring.mvc.entities.Beer;
+import com.spring.mvc.mappers.BeerMapper;
 import com.spring.mvc.model.BeerDTO;
 import com.spring.mvc.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -24,6 +25,26 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Autowired
+    BeerMapper beerMapper;
+
+
+    @Test
+    void updateExistingBeer() {
+        Beer beer = beerRepository.findAll().getFirst();
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(null);
+        final String beerName = "UPDATED";
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity responseEntity = beerController.updateById(beer.getId(), beerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+        assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+    }
 
     @Rollback
     @Transactional
