decbae056786efe6626adb3114f2773b7ddd5ec7
damiang <damiangildea12@gmail.com>
2025-03-08 11:27:04 +0000

    Added customer integration tests and serviceJPA implementation

---
+++ src/test/java/com/spring/mvc/controller/CustomerControllerIT.java
@@ -0,0 +1,57 @@
+package com.spring.mvc.controller;
+
+import com.spring.mvc.entities.Customer;
+import com.spring.mvc.model.CustomerDTO;
+import com.spring.mvc.repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class CustomerControllerIT {
+    @Autowired
+    CustomerController customerController;
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testCustomerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetCustomerById() {
+        Customer customer = customerRepository.findAll().getFirst();
+
+        CustomerDTO dto = customerController.getCustomerById(customer.getId());
+
+        assertThat(dto).isNotNull();
+    }
+
+    @Test
+    void testListCustomer() {
+        List<CustomerDTO> dtos = customerController.listCustomers();
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listCustomers();
+        assertThat(dtos.size()).isEqualTo(0);
+
+    }
+}
